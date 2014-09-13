232bc6979d8d1a17ef8b28227f865a9d34931d22
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-13 14:33:33 +1200

    added caching mechanism

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ var Xero = require('xero');
 var express = require('express');
 var xero = new Xero('NJRM3XEXDZDC6LSWFI4J9JREUD1IQK', 'QACOP5VZH6PBTTWFLAQCPPALRSG7JQ', fs.readFileSync('privatekey.pem'));
 var users;
+var invoices;
+var receipts;
+var payments;
 //var xerocall = 
 //app.use('/test', xerocall(req, res));
 var app = express();
@@ -23,29 +26,47 @@ app.get("/test", function (req, res) {
 });
 app.get("/Invoices", function (req, res) {
 	xero.call('GET', '/Invoices', null, function(err, json) {
-        if (err) {
-            log.error(err);
-            return res.json(400, {error: 'Unable to contact Xero'});
+        if(invoices == null){
+            if (err) {
+                log.error(err);
+                return res.json(400, {error: 'Unable to contact Xero'});
+            }
+            console.log('fetching');
+            invoices = json;;
+        }else{
+            console.log('using cache');
         }
-        return res.json(200, json);
+        return res.json(200, invoices);
     });
 });
 app.get("/Payments", function (req, res) {
     xero.call('GET', '/Payments', null, function(err, json) {
-        if (err) {
-            log.error(err);
-            return res.json(400, {error: 'Unable to contact Xero'});
+        if(payments == null){
+            if (err) {
+                log.error(err);
+                return res.json(400, {error: 'Unable to contact Xero'});
+            }
+            console.log('fetching');
+            payments = json;;
+        }else{
+            console.log('using cache');
         }
-        return res.json(200, json);
+        return res.json(200, payments);
     });
 });
 app.get("/Receipts", function (req, res) {
     xero.call('GET', '/Receipts', null, function(err, json) {
-        if (err) {
-            log.error(err);
-            return res.json(400, {error: 'Unable to contact Xero'});
+        if(receipts == null){
+            if (err) {
+                log.error(err);
+                return res.json(400, {error: 'Unable to contact Xero'});
+            }
+            console.log('fetching');
+            receipts = json;;
+        }else{
+            console.log('using cache');
         }
-        return res.json(200, json);
+        return res.json(200, receipts);
     });
 });
 app.listen(9090, function () { console.log("up on 9090") });
