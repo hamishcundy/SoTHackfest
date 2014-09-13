4bdf50d7065714425be2c1e9228a7b5000c6adb9
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-13 14:24:12 +1200

    added caching stuff

--- test.js
+++ test.js
@@ -2,16 +2,23 @@ var fs = require('fs');
 var Xero = require('xero');
 var express = require('express');
 var xero = new Xero('NJRM3XEXDZDC6LSWFI4J9JREUD1IQK', 'QACOP5VZH6PBTTWFLAQCPPALRSG7JQ', fs.readFileSync('privatekey.pem'));
+var users;
 //var xerocall = 
 //app.use('/test', xerocall(req, res));
 var app = express();
 app.get("/test", function (req, res) {
 	xero.call('GET', '/Users', null, function(err, json) {
-        if (err) {
-            log.error(err);
-            return res.json(400, {error: 'Unable to contact Xero'});
+        if(users == null){
+            if (err) {
+                log.error(err);
+                return res.json(400, {error: 'Unable to contact Xero'});
+            }
+            console.log('fetching');
+            users = res.json(200, json);
+        }else{
+            console.log('using cache');
         }
-        return res.json(200, json);
+        return users;
     });
 });
 app.get("/Invoices", function (req, res) {
