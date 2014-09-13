9875a578de42a628d070dc63e4eb20b372ea4b43
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-13 13:23:19 +1200

    stuff

--- example.js
+++ example.js
@@ -5,11 +5,12 @@ app.use('/', express.static('InvoiceTracker/'));
 
 var Xero = require('xero');
 var xero = new Xero('NJRM3XEXDZDC6LSWFI4J9JREUD1IQK', 'QACOP5VZH6PBTTWFLAQCPPALRSG7JQ', fs.readFileSync('privatekey.pem'));
-app.use('/test', xero.call('GET', '/Users', null, function(err, json) {
+var callback = xero.call('GET', '/Users', null, function(err, json) {
 	if(err){
 		log.error(err);
 		return res.json(400, {error: "Unable to contact Xero"});
 	}
 	return res.json(200, json);
-}));
+});
+app.use('/test', callback);
 app.listen(9090, function(){ console.log('listening')});
