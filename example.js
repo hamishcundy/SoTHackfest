7d6069d2bd3b41271c1457b0dc0a25cdef14ffa2
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-13 13:03:53 +1200

    Fixed example.js

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static('SoTHackfest/InvoiceTracker/'));
+app.use('/', express.static('InvoiceTracker/'));
 app.listen(9090, function(){ console.log('listening')});
