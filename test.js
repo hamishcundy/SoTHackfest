ef9435d04a25e9dc755fdb863e358d6b34383ef5
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-13 13:41:08 +1200

    test

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var Xero = require('xero');
 var xero = new Xero('NJRM3XEXDZDC6LSWFI4J9JREUD1IQK', 'QACOP5VZH6PBTTWFLAQCPPALRSG7JQ', fs.readFileSync('privatekey.pem'));
-xero.call('GET', '/Users', null, function(err, json) {
+xero.call('GET', '/Users', function(err, json) {
         if (err) {
             log.error(err);
             return res.json(400, {error: 'Unable to contact Xero'});
         }
         return res.json(200, json);
-    });
+    }, null);
