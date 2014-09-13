5ba0937a369ebd8e20e22fb30e77ca757be4e56e
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-13 17:42:27 +1200

    dialog

--- InvoiceTracker/js/main.js
+++ InvoiceTracker/js/main.js
@@ -1,1 +1,16 @@
 
+$( document ).ready(function() {
+
+
+$( ".item" ).click(function() {
+  $("#dialoguebox").css({ display: 'block' });
+  $("#closebox").css({ display: 'block' });
+});
+
+$( "#closebox" ).click(function() {
+  $("#dialoguebox").css({ display: 'none' });
+  $("#closebox").css({ display: 'none' });
+});
+
+
+});
