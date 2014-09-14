29b66c9f9f14e7095539fe668313b16d7a34fbba
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-14 15:52:08 +1200

    css

--- InvoiceTracker/js/main.js
+++ InvoiceTracker/js/main.js
@@ -4,4 +4,19 @@ $( document ).ready(function() {
 $('.spinner').animate({'opacity':'1.0'}, 300, 'linear');
 $(".spinner").css({ display: 'block' });
 
+var timeout = setTimeout(function()){
+
+	$("#logo").css({ display: 'block' });
+	$('#logo').animate({'opacity':'1.0'}, 300, 'linear');
+
+
+}, 200);
+
+var timeout = setTimeout(function()){
+
+	$("#logo").animate({margin-bottom: '+=50px'})
+
+}, 500);
+
 });
+
