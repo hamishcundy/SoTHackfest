d7840acbc1d56b20710137b851f5db327e126ec0
hamishcundy <cundyhami@myvuw.ac.nz>
2014-09-14 16:18:19 +1200

    safsadas

--- InvoiceTracker/js/main.js
+++ InvoiceTracker/js/main.js
@@ -14,9 +14,16 @@ var timeout = setTimeout(function(){
 
 var timeout = setTimeout(function(){
 
-	$("#logo").animate({bottom: '+=50px'});
+	$("#logo").animate({bottom: '+=25px'}), 300);;
 
 }, 500);
+
+var timeout = setTimeout(function(){
+	
+	$("#usersubcontainer").css({ display: 'block' });
+	$("#usersubcontainer").animate({'opacity':'1.0'}, 300, 'linear');
+
+}, 800);
 
 });
 
